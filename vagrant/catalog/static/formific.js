bd2f5d3994bef9d969f432b4b2cdaca61e9ddc4f
Yshia Wallace <yshiawallace@gmail.com>
2018-01-25 17:05:26 -0500

    Add style and functionality to flash message box

--- vagrant/catalog/static/formific.js
+++ vagrant/catalog/static/formific.js
@@ -1,4 +1,6 @@
 const navIcon = document.querySelector('.header__mobile .nav__icon');
+const alertBtn = document.querySelector('.alert-box__close');
+
 
 function toggleMobileMenu() {
 	const menu = document.querySelector('.header__mobile .nav');
@@ -7,7 +9,16 @@ function toggleMobileMenu() {
 	menu.classList.toggle('open');
 }
 
+function closeAlert() {
+	const alertMsg = document.querySelector('.alert-box__msg');
+	if (alertMsg.classList.contains('show')) {
+		alertMsg.classList.remove('show');
+		alertMsg.classList.add('hide');	
+	}
+}
+
 navIcon.addEventListener('click', toggleMobileMenu);
+alertBtn.addEventListener('click', closeAlert);
 
 
 
