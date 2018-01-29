63372fac2ccd55322004f2fe39290a5fa85cd301
Yshia Wallace <yshiawallace@gmail.com>
2018-01-29 17:26:28 -0500

    Move all routes to a module
    Create a module called 'views' and move all routes out of formific.py
    to this module. Register this module as a Blueprint and update all
    template URLs with this new module name.

--- vagrant/catalog/auth/controllers.py
+++ vagrant/catalog/auth/controllers.py
@@ -288,7 +288,7 @@ def disconnect():
         del login_session['user_id']
         del login_session['provider']
         flash("You have successfully been logged out.")
-        return redirect(url_for('showForms'))
+        return redirect(url_for('views.showForms'))
     else:
         flash("You were not logged in")
-        return redirect(url_for('showForms'))
+        return redirect(url_for('views.showForms'))
